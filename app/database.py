315a02374348741743dc8d5790d76f9032a2c660
kareem <kareemmahlees@gmail.com>
2022-07-31 18:31:15 +0200

    ✅ establishing connection with DB

---
+++ app/database.py
@@ -0,0 +1,22 @@
+import time
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from config import settings
+from rich.console import Console
+
+console = Console()
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            database=settings.database,
+            user=settings.user,
+            password=settings.password,
+            cursor_factory=RealDictCursor,
+        )
+        cr = conn.cursor()
+        break
+    except psycopg2.DatabaseError:
+        console.print("[bold red]FAILED : Connection To Database Failed")
+        print("Trying Again...")
+        time.sleep(3)
